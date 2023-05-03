339a60801f9990e1020bb2c9760a90039c51fa1e
Aditya Rifqy Fauzan <38560934+adityarifqyfauzan@users.noreply.github.com>
2023-05-03 17:06:04 +0700

    Nambahin say hello name

--- say_hello.go
+++ say_hello.go
@@ -3,3 +3,10 @@ package say_hello_modul
 func SayHello() string {
 	return "Hello World!"
 }
+
+func sayHelloName(name string) string {
+	if name != "" {
+		return "Hello " + name
+	}
+	return "Hello, World"
+}
